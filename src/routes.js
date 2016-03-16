e5230fa3ae11d8cb823d7ffadeb72590db5bd0f1
Dave Ramirez <ramirezd42@gmail.com>
2016-03-16 18:56:26 -0400

    added Mixer route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import LandingPage from './containers/LandingPage';
-import ChannelStrip from './common/components/ChannelStrip';
+import Mixer from './containers/Mixer';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute name="LandingPage" component={LandingPage} />
-    <Route path="channel-strip" name="ChannelStrip" component={ChannelStrip} />
+    <Route path="mixer" name="Mixer" component={Mixer} />
   </Route>
 );
